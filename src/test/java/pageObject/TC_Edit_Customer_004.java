0e631ab46fd58802920f220f6a576200468015f4
Brahmam <iAmBrams@Hotmail.com>
2023-02-11 09:40:02 +0530

    Added one modified file and others added

--- src/test/java/pageObject/TC_Edit_Customer_004.java
+++ src/test/java/pageObject/TC_Edit_Customer_004.java
@@ -1,2 +1,14 @@
 //This is a new Test cases
 //this test will check editing new customer
+
+package pageObject;
+
+import org.testng.annotations.Test;
+
+public class TC_Edit_Customer_004 {
+	
+	@Test
+	void getMessage() {
+		System.out.println("Hello, Get the Message...");
+	}
+}
---
+++ src/test/java/pageObject/justForTest.java
@@ -0,0 +1,12 @@
+package pageObject;
+
+import org.testng.annotations.Test;
+
+public class justForTest {
+
+	@Test
+	void justTestTheFile() {
+		System.out.println("Namaste Vizag");
+	}
+
+}
